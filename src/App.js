f096ef1c12b2c08215c02fb01fe21dbb12c17343
Alberto Garcia <albertogarcia2220@gmail.com>
2023-10-18 10:39:43 -0600

    Header and Footer Links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      hello nucamp!
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />}/>
+        <Route path='directory' element={<CampsitesDirectoryPage />}/>
+      </Routes>
+      <Footer />
     </div>
   );
 }
